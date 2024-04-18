1d27529bb50adb64ae9b5ca18b810fe2107eeca0
Jobayer Hosen <jobayerh99@gmail.com>
2024-04-18 13:04:49 +0600

    logical update

--- scripts/alpha.js
+++ scripts/alpha.js
@@ -1,4 +1,50 @@
-function continueGame(){
+
+
+function handleKeyboardKeyUpEvent(event) {
+    const playerPressed = event.key;
+    const currentAlphabetElement = document.getElementById('current-alphabet')
+    const currentAlphabet = currentAlphabetElement.innerText;
+    const expectedAlphabet = currentAlphabet.toLocaleLowerCase();
+    
+
+    if (playerPressed === expectedAlphabet){
+        continueGame()
+        removeBackgroundColourById(expectedAlphabet);
+
+        const currentScore = getTextElementValueById('current-score');
+        const updatedScore = currentScore + 1;
+        setTextElementValueById('current-score', updatedScore);
+
+        // const currentScoreElement = document.getElementById('current-score');
+        // const currentScoreText = currentScoreElement.innerText;
+        // const currentScore = parseInt(currentScoreText);
+
+        // const newScore = currentScore + 1;
+
+        // currentScoreElement.innerText = newScore;
+    }
+    else{
+        const currentLife = getTextElementValueById('current-life');
+        const updatedLife = currentLife - 1;
+        setTextElementValueById('current-life', updatedLife)
+
+    //    currentLifeElement = document.getElementById('current-life');
+    //    const currentLifeText = currentLifeElement.innerText;
+    //    const currentLife = parseInt(currentLifeText);
+
+    //    const newLife = currentLife - 1;
+
+    //    currentLifeElement.innerText = newLife;
+
+       if(newLife === 0){
+        gameOver();
+       }
+    }
+}
+
+document.addEventListener('keyup', handleKeyboardKeyUpEvent);
+
+function continueGame() {
     const alphabet = getARandomAlphabet();
 
     const currentAlphabetElement = document.getElementById('current-alphabet');
@@ -7,8 +53,17 @@ function continueGame(){
     setBackgroundColourById(alphabet);
 }
 
-function play(){
+function play() {
     hideElementById('home-screan');
+    hideElementById('final-score');
     showElementById('play-ground');
+
+
+
     continueGame()
+}
+
+function gameOver(){
+    hideElementById('play-ground');
+    showElementById('final-score');
 }
